9ed1a7c0627dd937350cffdd9395d0065205cd0a
Antonello Zanini <antozanini95@gmail.com>
2022-12-28 17:31:32 +0100

    libraries updated

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -3,8 +3,8 @@ export const SUPPORTED_LANGUAGES = ["en", "es", "it"]
 export type Language = typeof SUPPORTED_LANGUAGES[number]
 
 export type User = {
-    id: number,
+    id?: number,
     name: string,
     surname: string,
-    authenticationToken : string | null
+    authenticationToken?: string | null
 }
